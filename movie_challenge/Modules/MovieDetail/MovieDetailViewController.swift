8c9610eb82c810770001ad8eda70e53dcf1a8b7d
Hans Fehrmann <hans.jfehrmann@gmail.com>
2019-03-08 02:30:45 -0300

    Init movie detail

---
+++ movie_challenge/Modules/MovieDetail/MovieDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MovieDetailViewController.swift
+//  movie_challenge
+//
+//  Created by Laura Maldonado on 08-03-19.
+//  Copyright © 2019 Hans Fehrmann. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    @IBOutlet weak var movieLabel: UILabel!
+    @IBOutlet weak var movieImage: UIImageView!
+
+    @IBOutlet weak var popularityLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+
+}
